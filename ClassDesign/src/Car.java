4a3e1d3f59c1dc74516035aa0fd0bb524a5bfbc4
Cfinch99 <cfinch21@student.cccs.edu>
2024-08-23 12:02:20 -0600

    continued classwork on classes, objects, constructors, getters and setters

--- ClassDesign/src/Car.java
+++ ClassDesign/src/Car.java
@@ -1,4 +1,85 @@
 
 public class Car {
-
+	//data members
+	private String color;
+	private int mileage;
+	private boolean isClean;
+	private double amountOfGas;
+	private Wheel[] wheels = new Wheel[4];
+	
+	//constructors
+	public Car() {
+		color = "grey";
+		mileage = 0;
+		amountOfGas = 1.0;
+		isClean = true;
+		setWheelsOnCar();
+	}
+	public Car(String color, boolean isClean) {
+		this.color = color;
+		this.isClean = isClean;
+		setWheelsOnCar();
+	}
+	public Car(String color, int mileage, boolean isClean, double amountOfGas) {
+		super();
+		this.color = color;
+		this.mileage = mileage;
+		this.isClean = isClean;
+		this.amountOfGas = amountOfGas;
+		setWheelsOnCar();
+	}
+	
+	// getters and setters - encapsulate data members
+	public String getColor() {
+		return this.color;
+	}
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getMileage() {
+		return mileage;
+	}
+	public void setMileage(int mileage) {
+		this.mileage = mileage;
+	}
+	public boolean isClean() {
+		return isClean;
+	}
+	public void setClean(boolean isClean) {
+		this.isClean = isClean;
+	}
+	public double getAmountOfGas() {
+		return amountOfGas;
+	}
+	public void setAmountOfGas(double amountOfGas) {
+		this.amountOfGas = amountOfGas;
+	}
+	
+	public Wheel[] getWheels() {
+		return wheels;
+	}
+	public void setWheels(Wheel[] wheels) {
+		this.wheels = wheels;
+	}
+	
+	
+	//methods
+	
+	
+	
+	private void setWheelsOnCar() {
+		for (int i = 0; i < wheels.length; i++) {
+			wheels[i] = new Wheel(15, "Alloy");
+		}
+	}
+	
+	
+	@Override
+	public String toString() {
+		String str = " Color: " + color + "\n Mileage: " + mileage + "\n Clean: "
+	     + isClean + " \n Gallons of gas: " + amountOfGas;
+		return str;
+	}
+	
 }
--- ClassDesign/src/TestDriver.java
+++ ClassDesign/src/TestDriver.java
@@ -2,7 +2,10 @@
 public class TestDriver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Car myCar = new Car("pink", 5600, true, 12.5);
+		System.out.println(myCar);
+		
+		System.out.println(myCar.getWheels()[1]);
 
 	}
 
